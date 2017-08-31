511a10bd138c95c73271885eec639663d736b1e3
matianfu <lewis.ma@winsuntech.cn>
2017-08-31 16:07:45 +0800

    init release

--- README.md
+++ README.md
@@ -1,2 +1,52 @@
 # station-iso
-for station installation iso image
+This project builds wisnuc station installation iso image.
+
+It requires Ubuntu server amd64 version. The latest 16.04 LTS is officially supported. Other versions are not tested.
+
+# Usage
+
+1, Clone this repository and npm install.
+```
+$ git clone https://github.com/wisnuc/station-iso
+$ cd station-iso
+$ npm i
+```
+
+2, Download Ubuntu server amd64 iso image (keep the original file name).
+```
+// ubuntu official release
+$ wget http://releases.ubuntu.com/16.04.3/ubuntu-16.04.3-server-amd64.iso
+
+or alternatively,
+
+// 163.com mirror in china
+$ wget http://mirrors.163.com/ubuntu-releases/16.04.3/ubuntu-16.04.3-desktop-amd64.iso
+```
+
+3, Build the iso image. `sudo` is required for it mounts iso as loop device.
+```
+$ sudo node index.js ubuntu-16.04.3-server-amd64.iso
+
+or
+
+$ sudo npm start ubuntu-16.04.3-server-amd64.iso
+```
+
+It generates an iso file named such as `ubuntu-ubuntu-16.04.3-server-amd64-wisnuc-station-0.8.1.iso`.
+
+# Options
+
+By default, the script pulls the latest wisnuc station release from github, including beta release.
+
+You can specify the release `tag_name` with `-v` option. Then the specified version of wisnuc station will be included in the iso image.
+
+Example:
+
+```
+sudo node index.js ubuntu-16.04.3-server-amd64.iso -v 0.9.1
+```
+
+# Help
+
+File a issue for question, feature request, or bug report.
+
---
+++ index.js
@@ -0,0 +1,149 @@
+const Promise = require('bluebird')
+const path = require('path')
+const fs = Promise.promisifyAll(require('fs'))
+const child = Promise.promisifyAll(require('child_process'))
+const request = require('superagent')
+
+const { log } = console
+
+// core function
+// 1. if string, run it with child.spawn
+// 2. if sync function, it is invoked to generate command line, aka, lazy
+// 3. if async function, it is invoked with await
+// 4. if array, then all entries are paralleled with promise.all
+const spawnCommandAsync = async command => {
+  if (typeof command === 'string') {
+    return new Promise((resolve, reject) => {
+      let cmds = command.trim().split(' ').filter(x => x.length > 0)
+      let finished = false
+      log(':: ', command)  
+      let spawn = child.spawn(cmds[0], cmds.slice(1), { stdio: 'inherit' })
+      spawn.on('error', err => (finished = true, reject(err)))
+      spawn.on('exit', (code, signal) => {
+        if (finished) return
+        if (code || signal) {
+          reject(new Error(`${cmds[0]} exit with code ${code} and signal ${signal}`))
+        } else {
+          resolve()
+        }
+      })
+    })
+  } else if (typeof command === 'function') {
+    if (command.constructor.name === 'AsyncFunction') {
+      return command()
+    } else {
+      return spawnCommandAsync(command())
+    }
+  } else if (Array.isArray(command)) {
+    if (command.par) {
+      return Promise.all(command.map(cmd => spawnCommandAsync(cmd)))
+    } else {
+      for (let i = 0; i < command.length; i++) {
+        await spawnCommandAsync(command[i])
+      }
+    }
+  }
+}
+
+// stamp par on array (for parallel)
+const par = array => (array.par = true, array)
+
+// global variable
+let release
+
+// constant
+const addr = 'https://raw.githubusercontent.com'
+const updateUrl = `${addr}/wisnuc/appifi-bootstrap-update/release/appifi-bootstrap-update.packed.js`
+const bootstrapUrl = `${addr}/wisnuc/appifi-bootstrap/release/appifi-bootstrap.js.sha1`
+const nodeUrl = 'https://nodejs.org/dist/v8.4.0/node-v8.4.0-linux-x64.tar.xz'
+
+// mandatory arg
+const iso = process.argv.find(arg => arg.endsWith('.iso'))
+// optional arg (-v)
+const version = process.argv.find((arg, index, array) => (index !== 0 && array[index - 1] === '-v'))
+
+if (!iso) {
+  log('please provide iso file path')
+  process.exit(1)
+}
+
+// retrieve release object
+const retrieveAsync = async () => {
+  let releases = await new Promise((resolve, reject) => request
+    .get('https://api.github.com/repos/wisnuc/appifi-release/releases')
+    .end((err, res) => err ? reject(err) : resolve(res.body)))
+
+  if (version) {
+    release = releases.find(rel => rel.tag_name === version)
+    if (!release) {
+      log('version not found')
+      process.exit(1)
+    }
+  } else {
+    if (releases.length === 0) {
+      log('no available release')
+      process.exit(1)
+    }
+    release = releases[0] 
+  }
+}
+
+// volume id for iso image
+const volumeId = () => (/^ubuntu-[0-9]{2}.[0-9]{2}(.[0-9])?-server-amd64.iso$/.test(iso)) 
+  ? `ubuntu-${iso.length === 29 ? iso.substring(7, 12) : iso.substring(7, 14)}-wisnuc` 
+  : `ubuntu-wisnuc`
+
+// output iso file name
+const output = () => path.basename(iso, '.iso') + `-wisnuc-station-${release.tag_name}.iso`
+
+// repacked tarball file name
+const repacked = () => `appifi-${release.tag_name}-${release.id}-${release.target_commitish.slice(0,8)}-${(release.prerelease ? 'pre' : 'rel')}.tar.gz`
+
+// append to ubuntu-server.seed
+const seed = () => ` 
+# Individual additional packages to install
+d-i pkgsel/include string avahi-daemon avahi-utils btrfs-tools imagemagick ffmpeg samba udisks2
+# Install wisnuc files
+d-i preseed/late_command string \\
+mkdir -p /target/wisnuc/appifi-tarballs; \\
+mkdir -p /target/wisnuc/bootstrap; \\
+cp /cdrom/wisnuc/${repacked()} /target/wisnuc/appifi-tarballs; \\
+cp /cdrom/wisnuc/appifi-bootstrap-update.packed.js /target/wisnuc/bootstrap; \\
+cp /cdrom/wisnuc/appifi-bootstrap.js.sha1 /target/wisnuc/bootstrap; \\
+cp /cdrom/wisnuc/appifi-bootstrap-update.service /target/lib/systemd/system; \\
+cp /cdrom/wisnuc/appifi-bootstrap-update.timer /target/lib/systemd/system; \\
+cp /cdrom/wisnuc/appifi-bootstrap.service /target/lib/systemd/system; \\
+cp /cdrom/wisnuc/${nodeUrl.split('/').pop()} /target; \\
+in-target tar xJf ${nodeUrl.split('/').pop()} -C /usr --strip-components=1; \\
+rm -rf /target/${nodeUrl.split('/').pop()}; \\
+in-target systemctl stop smbd nmbd; \\
+in-target systemctl disable smbd nmbd; \\
+in-target systemctl enable appifi-bootstrap; \\
+in-target systemctl enable appifi-bootstrap-update.timer`
+
+// series
+spawnCommandAsync([
+  retrieveAsync,
+  'rm -rf tmp iso cd-image',
+  'mkdir -p tmp iso cd-image/wisnuc',
+  `mount -o loop ${iso} iso`,
+  'cp -rT iso cd-image',
+  'umount iso',
+  'chmod a+w cd-image/preseed/ubuntu-server.seed',
+  'chmod a+w cd-image/isolinux/isolinux.bin',
+  'cp assets/appifi-bootstrap-update.service cd-image/wisnuc',
+  'cp assets/appifi-bootstrap-update.timer cd-image/wisnuc',
+  'cp assets/appifi-bootstrap.service cd-image/wisnuc',
+  `wget -O cd-image/wisnuc/${nodeUrl.split('/').pop()} ${nodeUrl}`,
+  `wget -O cd-image/wisnuc/appifi-bootstrap-update.packed.js ${updateUrl}`,
+  `wget -O cd-image/wisnuc/appifi-bootstrap.js.sha1 ${bootstrapUrl}`,
+  () => `wget -O cd-image/wisnuc/appifi-${release.tag_name}-orig.tar.gz ${release.tarball_url}`,
+  () => `tar xzf cd-image/wisnuc/appifi-${release.tag_name}-orig.tar.gz -C tmp --strip-components=1`,  
+  async () => (log('write tmp/.release.json'), fs.writeFileAsync('tmp/.release.json', JSON.stringify(release, null, '  '))),
+  async () => (log('write cd-image/.release.json'), fs.writeFileAsync('cd-image/.release.json', JSON.stringify(release, null, '  '))),
+  () => `tar czf cd-image/wisnuc/${repacked()} -C tmp .`,
+  async () => (log('append ubuntu-server.seed'), log(seed()), fs.writeFileAsync('cd-image/preseed/ubuntu-server.seed', seed(), { flag: 'a' })),
+  () => `mkisofs -r -V "${volumeId()}" -cache-inodes -J -l -b isolinux/isolinux.bin -c isolinux/boot.cat -no-emul-boot -boot-load-size 4 -boot-info-table -o ${output()} cd-image`,
+]).then(x => x, e => console.log(e))
+
+
